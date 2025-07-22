1363c2510a189c7fc1e597728861f5f6b32bea23
mahandaemi <mahandaemi1234@gmail.com>
2025-07-22 23:28:02 +0330

    Update 01-LED.ino

--- 01-LED/01-LED.ino
+++ 01-LED/01-LED.ino
@@ -1,9 +1,12 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(12,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(12,1);
+  delay(100);
+  digitalWrite(12,0);
+  delay(100);
+  
 }
